0b170124a3fb8c17a3347cabc1278a9b67f60515
monicaortiz20 <ortiznavajomonica@gmail.com>
2022-08-16 21:28:25 +0200

    ADD/continue with FirebaseStore

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
 // ------------------ FIREBASE -----------------//
 
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyA4n8rngi4aJiTimJAV1ZqEj6t7bqhbShU",
-    authDomain: "nyt-best-sellers-6ae9f.firebaseapp.com",
-    projectId: "nyt-best-sellers-6ae9f",
-    storageBucket: "nyt-best-sellers-6ae9f.appspot.com",
-    messagingSenderId: "642446888824",
-    appId: "1:642446888824:web:de443e5e4a271322f4c28d"
+   // Your web app's Firebase configuration
+   const firebaseConfig = {
+    apiKey: "AIzaSyB2ETajR1cx8tzZlNXZjy9cgqbn0I2cvGM",
+    authDomain: "nyt-bestsellers-c5110.firebaseapp.com",
+    projectId: "nyt-bestsellers-c5110",
+    storageBucket: "nyt-bestsellers-c5110.appspot.com",
+    messagingSenderId: "857805696161",
+    appId: "1:857805696161:web:95a688f640509d901d14e0"
   };
+
 
   // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
@@ -16,59 +17,84 @@
     //para llamar a la bbdd
     const db = firebase.firestore();
 
-    //Creación de la colección "users"
-    const createUser = (user) => {
-        db.collection("users")
-            .add(user)
-            .then((docRef) => console.log("Document written with ID: ", docRef.id))
-            .catch((error) => console.error("Error adding document: ", error));
-    };
+    db.collection("users").add({
+        first: "Ada",
+        last: "Lovelace",
+        born: 1815
+    })
+        .then((docRef) => {
+            console.log("Document written with ID: ", docRef.id);
+        })
+        .catch((error) => {
+            console.error("Error adding document: ", error);
+        });
+
 
 
     //Auth Firebase con Google
-    let provider = new firebase.auth.GoogleAuthProvider();
+    // let provider = new firebase.auth.GoogleAuthProvider();
     //coleccion users
-    const collect = db.collection('users').doc();
     
     //función para logarse con Google
-    async function loginGoogle(){
-       try {
-            const response = await firebase.auth().signInWithPopup(provider);
-            console.log(response)
-            if (response.user) {
-                document.getElementById('login').style.display = "none";
-                document.getElementById('logout').style.display = "none";
+    // async function loginGoogle(){
+    //    try {
+    //         const response = await firebase.auth().signInWithPopup(provider);
+    //         console.log(response)
+    //         if (response.user) {
+    //             document.getElementById('login').style.display = "none";
+    //             document.getElementById('logout').style.display = "none";
 
-                // Usuario registrado
-                const user = document.getElementById('usuario')
-                const name = document.createElement("p")
+    //             // Usuario registrado
+    //             const user = document.getElementById('usuario')
+    //             const name = document.createElement("p")
 
-                user.append(name)
-                name.innerHTML = `User ${response.additionalUserInfo.profile.given_name}`
+    //             user.append(name)
+    //             name.innerHTML = `User ${response.additionalUserInfo.profile.given_name}`
 
-                //Añadir usuario a la bbdd
-                collect.set({
-                    Name: response.additionalUserInfo.profile.given_name,
-                    Email: response.additionalUserInfo.profile.email,
-                    ID: collect.id
-                })
-            }
+    //             //Añadir usuario a la bbdd
+    //             collect.set({
+    //                 Name: response.additionalUserInfo.profile.given_name,
+    //                 Email: response.additionalUserInfo.profile.email,
+    //                 ID: collect.id
+    //             })
+    //         }
         
-       } catch (error) {
-            throw new Error(error);
-       }
-    }
+    //    } catch (error) {
+    //         throw new Error(error);
+    //    }
+    // }
+
+    // async function loginGoogle(){
+    //     try {
+    //         await firebase.auth().signInWithPopup(provider).then((response) => {
+    //             console.log(response)
+    //             let user = [
+    //                 {
+    //                     "Name": response.user.name,
+    //                     "Email": response.user.email,
+    //                     "ID": response.id
+    //                 }
+    //               ];
+    //               console.log(user);
+    //               localStorage.setItem("user", JSON.stringify(user));
+    //         }).catch((error) => {
+    //             console.log(error)
+    //         })
+    //     } catch (error) {
+    //         console.log(error)
+    //     }
+    // }
 
 
 
-    //Botón para logarse con Google
-    const googleBtn = document.getElementById('login')
+    // //Botón para logarse con Google
+    // const googleBtn = document.getElementById('login')
 
-    googleBtn.addEventListener('click', async (event) => {
-        try {
-            await loginGoogle()
-        } catch (error) {}
-    })
+    // googleBtn.addEventListener('click', async (event) => {
+    //     try {
+    //         await loginGoogle()
+    //     } catch (error) {}
+    // })
 
 
 
