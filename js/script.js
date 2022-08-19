3a50450f97a71e787e6bc4b8bc94ded718ccbc8a
monicaortiz20 <ortiznavajomonica@gmail.com>
2022-08-19 12:21:39 +0200

    ADD/functions for SignUp/logIn/logOut, first fetch

--- js/script.js
+++ js/script.js
@@ -44,6 +44,33 @@ const saveUsers = (email, password) =>
 
 
 
+//----------------- Botones registro e inicio sesión -----------------//
+
+const registered = document.getElementById('registered-btn')
+const iniciated = document.getElementById('iniciate-btn')
+
+document.getElementById('signup-section').style.display = "none";
+document.getElementById('signin-section').style.display = "none";
+document.getElementById('logout').style.display = "none"
+
+
+registered.addEventListener('click', e => {
+  e.preventDefault();
+  document.getElementById('signup-section').style.display = "flex";
+  document.getElementById('signin-section').style.display = "none";
+
+})
+
+iniciated.addEventListener('click', e => {
+  e.preventDefault();
+  document.getElementById('signup-section').style.display = "none";
+  document.getElementById('signin-section').style.display = "flex";
+})
+
+
+
+
+
 //----------------- funciones para signUp -----------------//
 const signupForm = document.getElementById('signup-form');
 
@@ -53,15 +80,17 @@ signupForm.addEventListener('submit', (e) => {
   //valor inputs
   const email = document.getElementById('signup-mail').value;
   const password = document.getElementById('signup-password').value;
+
   
   auth
   .createUserWithEmailAndPassword(email, password)
   .then(userCredential => {
-
+    
     //limpiamos el form
     signupForm.reset();
     console.log('usuario registrado')
   })
+  
 })
 
 
@@ -108,6 +137,11 @@ const tablon = document.getElementById('tablon');
 auth.onAuthStateChanged(user =>{
   if (user) {
     console.log('auth: OK, sign in')
+    document.getElementById('signup-section').style.display = "none";
+    document.getElementById('signin-section').style.display = "none";
+    signupForm.style.display = "none"
+    signinForm.style.display = "none"
+    document.getElementById('logout').style.display = "flex"
   }
 })
 
@@ -122,48 +156,48 @@ auth.onAuthStateChanged(user =>{
 
 // -------------- GESTIÓN ENTRADAS ----------------//
 
-// async function bringEntries () {
-//     try {
-//         let responseBooks = await fetch(`https://api.nytimes.com/svc/books/v3/lists/names.json?api-key=6UgXRIVdy52ixbIvS1lEzoGQUOfC6qQ7`);
-//         let data = await responseBooks.json()
-//         const entry = document.getElementsByClassName("entry")[0];
-//         entry.style.display="none";
-//         return data
+async function bringEntries () {
+    try {
+        let responseBooks = await fetch(`https://api.nytimes.com/svc/books/v3/lists/names.json?api-key=6UgXRIVdy52ixbIvS1lEzoGQUOfC6qQ7`);
+        let data = await responseBooks.json()
+        const entry = document.getElementsByClassName("entry")[0];
+        entry.style.display="none";
+        return data
         
-//     } catch (error) {
-//         console.error(error);
-//     }
-// }
+    } catch (error) {
+        console.error(error);
+    }
+}
 
-// bringEntries().then(function bringBooks(books){
-//     let categories = books.results
+bringEntries().then(function bringBooks(books){
+    let categories = books.results
 
-//     //Para cada categoría:
-//     categories.forEach((category, i) => {
+    //Para cada categoría:
+    categories.forEach((category, i) => {
 
-//         //creamos tarjetas:
-//         let h3 = document.createElement("h3");
-//         let pOld = document.createElement("p");
-//         let pNew = document.createElement("p");
-//         let pUpdate = document.createElement("p");
-//         let btn = document.createElement("button");
-//         let div = document.createElement("div");
-//         let tablon = document.getElementById("tablon");
+        //creamos tarjetas:
+        let h3 = document.createElement("h3");
+        let pOld = document.createElement("p");
+        let pNew = document.createElement("p");
+        let pUpdate = document.createElement("p");
+        let btn = document.createElement("button");
+        let div = document.createElement("div");
+        let tablon = document.getElementById("tablon");
 
-//         div.append(h3,pOld,pNew,pUpdate,btn)
-//         tablon.appendChild(div)
+        div.append(h3,pOld,pNew,pUpdate,btn)
+        tablon.appendChild(div)
 
-//         //añadimos info de cada uno
-//         h3.innerHTML = category.display_name
-//         pOld.innerHTML = `Oldest: ${category.oldest_published_date}`
-//         pNew.innerHTML = `Newest: ${category.newest_published_date}`
-//         pUpdate.innerHTML = `Updated: ${category.updated}`
-//         btn.innerHTML = "READ MORE"
+        //añadimos info de cada uno
+        h3.innerHTML = category.display_name
+        pOld.innerHTML = `Oldest: ${category.oldest_published_date}`
+        pNew.innerHTML = `Newest: ${category.newest_published_date}`
+        pUpdate.innerHTML = `Updated: ${category.updated}`
+        btn.innerHTML = "READ MORE"
 
-//         //estilos:
-//         h3.classList = "title"
-//         btn.classList = "button"
-//         div.classList = "divCategories"
+        //estilos:
+        h3.classList = "title"
+        btn.classList = "button"
+        div.classList = "divCategories"
 
-//     });
-// })
+    });
+})
